aba2e849f65c8a61bad1855a219f5d026e526c84
Yashraj <yeshrajteli12345@gmail.com>
2023-09-11 22:03:31 +0530

    gtag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,22 @@ import HowItWorks from '@/components/HowItWorks';
 import { MainSection } from '@/components/MainSection'
 import Order from '@/components/Order';
 import TiffinPicks from '@/components/TiffinPicks';
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-1HFXCRPML6" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1HFXCRPML6');
+        `}
+      </Script>
+
       <Hero />
       <MainSection />
       <TiffinPicks />
