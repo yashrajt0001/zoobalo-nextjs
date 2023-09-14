ea23703bd3bc14c012e6db4827e91abf9e07e7ac
Yashraj <yeshrajteli12345@gmail.com>
2023-09-14 16:25:51 +0530

    minor style change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       {/* NavBar */}
 
       <div>
-        <div className="bg-white border-b border-slate-200 sticky top-0 flex justify-between px-12 py-6 z-10">
+        <div className="bg-white border-b border-slate-200 sticky top-0 flex justify-between px-8 sm:px-12 py-6 z-10">
           <div className="flex items-center justify-center">
             <Image src={logo1} alt="logo" className="w-28 h-12" />
             <div className="hidden sm:flex items-center  gap-10 ml-12 text-lg font-medium">
