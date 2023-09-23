28cb628d2cf9e30ea9fbb054c127795ead4cd513
Yashraj <yeshrajteli12345@gmail.com>
2023-09-23 22:33:40 +0530

    added some api

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { FormEvent, useState } from "react";
 import Image from "next/image";
-import whatsapp from "../../assets/images/whatsapp.png";
 import logo1 from "../../assets/images/logo1.png";
 import Link from "next/link";
+import axios from "axios";
 
 const page = () => {
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    balance: "",
+  });
+  const [delBoyDetails, setDelBoyDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [showError, setShowError] = useState<undefined | string>(undefined);
+
+  const handleUserSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (
+      !userDetails.name ||
+      !userDetails.address ||
+      !userDetails.phone ||
+      userDetails.balance == undefined
+    ) {
+      return setShowError("Please enter details!");
+    }
+
+    await axios.post(
+      "http://localhost:5000/createUser",
+      {
+        name: userDetails.name,
+        address: userDetails.address,
+        phone: userDetails.phone,
+        balance: userDetails.balance,
+      },
+      {
+        headers: {
+          'auth-token': localStorage.getItem('auth-token')
+        },
+      }
+    );
+
+    setUserDetails({name: '', address: '', balance: '', phone: ''})
+  };
+
+  const handleDelBoySubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (
+      !userDetails.name ||
+      !userDetails.address ||
+      !userDetails.phone ||
+      !userDetails.balance
+    ) {
+      setShowError("Please enter details!");
+    }
+  };
   return (
     <div>
       <div className="bg-white border-b border-slate-200 sticky top-0 flex justify-between px-8 sm:px-12 py-6 z-10">
@@ -14,39 +70,72 @@ const page = () => {
             <Link href="/users">Users</Link>
           </div>
         </div>
-        <div className="">
-          <a
-            href="https://wa.me/919509919001"
-            className="text-white py-3 px-4 bg-[#FF5823] rounded-2xl flex"
-          >
-            <Image src={whatsapp} alt="logo" className="w-6 h-6 mr-3" />
-            Book Now
-          </a>
-        </div>
       </div>
       <h1 className="text-4xl mt-5 ml-12 text-[#FF5F1F]">
         Hi! <span className="text-green-500">Admin</span>{" "}
       </h1>
+      {showError && (
+        <div className="text-red-500 ml-12 text-2xl mt-4">{showError}</div>
+      )}
       <div className="flex">
-        <div className="ml-16 flex flex-col gap-3 w-[40%]">
+        <form
+          onSubmit={handleUserSubmit}
+          className="ml-16 flex flex-col gap-3 w-[40%]"
+        >
           <h1 className="text-3xl mt-5">Create a User:</h1>
           <input
             type="text"
+            value={userDetails.name}
+            name="name"
+            onChange={(e) => {
+              setShowError(undefined);
+              setUserDetails({
+                ...userDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="Name"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
           <input
             type="text"
+            value={userDetails.address}
+            name="address"
+            onChange={(e) => {
+              setShowError(undefined);
+              setUserDetails({
+                ...userDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="Address"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
           <input
             type="tel"
+            value={userDetails.phone}
+            name="phone"
+            onChange={(e) => {
+              setShowError(undefined);
+              setUserDetails({
+                ...userDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="Phone Number"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
           <input
-            type="tel"
+            type="number"
+            value={userDetails.balance}
+            name="balance"
+            onChange={(e) => {
+              setShowError(undefined);
+              setUserDetails({
+                ...userDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="User's Balance"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
@@ -56,22 +145,52 @@ const page = () => {
           >
             Create
           </button>
-        </div>
+        </form>
 
-        <div className="ml-24 flex flex-col gap-3 w-[40%]">
+        <form
+          onSubmit={handleDelBoySubmit}
+          className="ml-24 flex flex-col gap-3 w-[40%]"
+        >
           <h1 className="text-3xl mt-5">Create a Delivery Boy Account:</h1>
           <input
             type="text"
+            name="name"
+            value={delBoyDetails.name}
+            onChange={(e) => {
+              setShowError(undefined);
+              setDelBoyDetails({
+                ...delBoyDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="Name"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
           <input
             type="email"
+            name="email"
+            value={delBoyDetails.email}
+            onChange={(e) => {
+              setShowError(undefined);
+              setDelBoyDetails({
+                ...delBoyDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="Email Address"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
           <input
             type="password"
+            name="password"
+            value={delBoyDetails.password}
+            onChange={(e) => {
+              setShowError(undefined);
+              setDelBoyDetails({
+                ...delBoyDetails,
+                [e.target.name]: e.target.value,
+              });
+            }}
             placeholder="Password"
             className=" p-5 outline-none border-[2px] border-gray-200 rounded-lg"
           />
@@ -81,7 +200,7 @@ const page = () => {
           >
             Create
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,15 +2,20 @@
 
 import { Card } from '@/components/Card';
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 
 const page = () => {
-
   const [users, setUsers] = useState([]);
 
-  const getUsers = async () => {
-      const res = await axios.get('')
-  }
+  useEffect(() => {
+    const getUsers = async () => {
+      const {data} = await axios.get('http://localhost:5000/getAllUsers')
+      setUsers(data)
+    }
+
+    getUsers()
+  }, [])
+  
 
   return (
     <div className='ml-10'>
