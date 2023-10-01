15f0da4a15856e4dc9eaf1f28ce56f8e0432d6cd
Yashraj <yeshrajteli12345@gmail.com>
2023-10-01 23:32:57 +0530

    debug

--- src/app/api/setPriority/route.ts
+++ src/app/api/setPriority/route.ts
@@ -14,16 +14,15 @@ export async function POST(req: Request) {
     const body = await req.json();
     console.log(body)
     await prisma.today_delivery.deleteMany({});
-    body.map(async (item: any) => {
+    await body.map((item: any) => {
       console.log('func ran')
-      const today_delivery = await prisma.today_delivery.create({
+      prisma.today_delivery.create({
         data: {
           userId: parseInt(item.userId),
           priority: parseInt(item.priority),
           isDelivered: false,
         },
       });
-      console.log(today_delivery)
     });
     return new Response("OK");
   // } catch (error: any) {
