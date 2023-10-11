460268aa5fb83023cc2de9928a791102d67406e7
Yashraj <yeshrajteli12345@gmail.com>
2023-10-11 21:23:04 +0530

    bug fix

--- src/app/api/setPriority/route.ts
+++ src/app/api/setPriority/route.ts
@@ -3,8 +3,10 @@ import jwt from "jsonwebtoken";
 const jwtSecret = "secret123";
 const { verify } = jwt;
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
-  // try {
+  try {
     const authToken = req.headers.get("auth-token");
     if (!authToken) {
       return new Response("please provide auth token", { status: 400 });
@@ -16,10 +18,10 @@ export async function POST(req: Request) {
     await prisma.today_delivery.deleteMany({});
     await body.map(async(item: any) => {
       console.log('func ran')
-      await prisma.$executeRaw`insert into today_delivery (priority, userId, isDelivered) values (${item.priority}, ${item.userId}, false)`
+      await prisma.$executeRaw`insert into today_delivery (priority, userId, isDelivered) values (${parseInt(item.priority)}, ${parseInt(item.userId)}, false)`
     });
     return new Response("OK");
-  // } catch (error: any) {
-  //   return new Response(error, { status: 400 });
-  // }
+  } catch (error: any) {
+    return new Response(error, { status: 400 });
+  }
 }
